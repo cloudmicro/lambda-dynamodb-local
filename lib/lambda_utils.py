a237ec70edc16046672d2a2e21b2272485dcbf60
Patrick Brandt <l15486@WPELL15486.hsd1.ga.comcast.net>
2016-01-23 13:43:47 -0500

    adding docstrings

--- lib/lambda_utils.py
+++ lib/lambda_utils.py
@@ -5,16 +5,21 @@ logger.setLevel(logging.INFO)
 
 
 def get_env(context):
+    """This helper function returns the AWS Lambda function Alias for the current Lambda function"""
+    # TODO: handle Lambda functions that don't have an alias
     split_arn = context.invoked_function_arn.split(':')
     env = split_arn[len(split_arn) - 1]
     return env
 
 
 def import_config(f):
+    """
+    Apply this directive to an AWS Lambda function handler to inject an alias-specific config object
+    """
     def wrapper(*args, **kwargs):
         context = args[1]
         env = get_env(context)
-        logger.info('environemnt: {}'.format(env))
+        logger.info('environment: {}'.format(env))
 
         config = __import__(env + '-config')
         logger.info('imported config: {}'.format(config))
